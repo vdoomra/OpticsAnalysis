79ad150e1cb896e3a820f39e0d46531de3e10787
vdoomra <68244726+vdoomra@users.noreply.github.com>
2025-09-29 13:16:44 -0700

    Update gem_dist_plots.C

--- GEM Distributions/gem_dist_plots.C
+++ GEM Distributions/gem_dist_plots.C
@@ -6,7 +6,8 @@
 const int nFiles = 1;
 const int nsectors = 7;
 
-const char* fin[nFiles] = {"../slim_output/slim_moller_with_sieve_p5.root"};
+const char* fin_moller[nFiles] = { "slim_output/slim_sieve_C12_opticsDS_p2_500.root"};
+const char* fin_C12[nFiles] = {"slim_output/slim_sieve_C12_opticsDS_p2_500.root"};
 
 const char* passNames[nFiles] = {"pass5"};
 const double pi = acos(-1);
@@ -18,113 +19,220 @@ const char* sector[nsectors] = {"sector1", "sector2", "sector3", "sector4", "sec
 double phi_lo[nsectors] = {0, 2*pi/7, 4*pi/7, 6*pi/7, 8*pi/7, 10*pi/7, 12*pi/7};
 double phi_hi[nsectors] = {2*pi/7, 4*pi/7, 6*pi/7, 8*pi/7, 10*pi/7, 12*pi/7, 2*pi};
 
-TH2D* hist_r_phi[nFiles] = {NULL};
-TH2D* hist_rprime_phi[nFiles] = {NULL};
-TH2D* hist_phiprime_phi[nFiles] = {NULL};
-TH2D* Etheta[nFiles] = {NULL};  // E vs theta_tg
+TH2D* h2d_r_phi_C12[nFiles] = {NULL};
+TH2D* h2d_rprime_phi_C12[nFiles] = {NULL};
+TH2D* h2d_phiprime_phi_C12[nFiles] = {NULL};
 
-TH1D* radial[nFiles][nsectors] = {NULL};
-TH1D* phi_dist[nFiles][nsectors] = {NULL};
-TH1D* rprime[nFiles][nsectors] = {NULL};
+TH1D* main_radial_C12[nFiles] = {NULL};
+TH1D* main_radial_moller[nFiles] = {NULL};
 
-TCanvas* c[nFiles] = {NULL};
+TH2D* h2d_r_phi_moller[nFiles] = {NULL};
+TH2D* h2d_rprime_phi_moller[nFiles] = {NULL};
+TH2D* h2d_phiprime_phi_moller[nFiles] = {NULL};
 
-void gem_dist_plots(){
+TH2D* Etheta_C12[nFiles] = {NULL};  // E vs theta_tg
+TH2D* Etheta_moller[nFiles] = {NULL};  // E vs theta_tg
+
+TH1D* radial_C12[nFiles][nsectors] = {NULL};
+TH1D* rprime_C12[nFiles][nsectors] = {NULL};
+
+TH1D* radial_moller[nFiles][nsectors] = {NULL};
+TH1D* rprime_moller[nFiles][nsectors] = {NULL};
+
+TCanvas* c1[nFiles] = {NULL};
+TCanvas* c2[nFiles] = {NULL};
+TCanvas* c3[nFiles] = {NULL};
+
+void moller_C12(){
 
   for(int i=0; i<nFiles; i++){
 
-    TFile* f1 = new TFile(Form("%s",fin[i]), "READ");
+    TFile* f1 = new TFile(Form("%s",fin_C12[i]), "READ");
+    TFile* f2 = new TFile(Form("%s",fin_moller[i]), "READ");
 
     TTree* T1 = (TTree*)f1->Get("newT");
+    TTree* T2 = (TTree*)f2->Get("newT");
 
-    c[i] = new TCanvas(Form("Canvas_%s",passNames[i]));
+    c1[i] = new TCanvas(Form("C1_%s",passNames[i]));
+    c2[i] = new TCanvas(Form("C2_%s",passNames[i]));
+    c3[i] = new TCanvas(Form("C3_%s",passNames[i]));
 
-    int nevents = T1->GetEntries();
+    int nevents_c12 = T1->GetEntries();
 
-    hist_r_phi[i] = new TH2D(Form("gem_r_phi_%s",passNames[i]), Form("gem_dist_%s;r[mm];#phi[rad]",passNames[i]), 600, 500, 1100, 500, 0, 2*pi); 
-    hist_rprime_phi[i] = new TH2D(Form("gem_rprime_phi_%s",passNames[i]), Form("gem_dist_%s;r[mm];#phi[rad]",passNames[i]), 500, 0, 0.1, 500, 0, 2*pi); 
-    hist_phiprime_phi[i] = new TH2D(Form("gem_phiprime_phi_%s",passNames[i]), Form("gem_dist_%s;r[mm];#phi[rad]",passNames[i]), 500, -0.02, 0.02, 500, 0, 2*pi); 
-    Etheta[i] = new TH2D(Form("Etheta_%s",passNames[i]),"E' vs #theta;#theta_{tg}(deg);E'(GeV)",100,0,1.8,100,0,12);
-    
+    main_radial_C12[i] = new TH1D("main_r_C12","r_distribution;Counts",500, 500, 1200); 
+    main_radial_moller[i] = new TH1D("main_r_moller","r_distribution;Counts",500, 500, 1200); 
+
+    h2d_r_phi_C12[i] = new TH2D("r_phi_C12","r_phi_distribution;phi[rad];Counts",500, 500, 1100, 500, 0, 2*pi); 
+    h2d_rprime_phi_C12[i] = new TH2D("rprime_phi_C12","rprime_phi_distribution;phi[rad];Counts", 500, 0 , 0.1, 200, 0, 2*pi);
+    h2d_phiprime_phi_C12[i] = new TH2D("phiprime_phi_C12","phiprime_phi_distribution;phi[rad];Counts", 500, -0.02 , 0.02, 200, 0, 2*pi);
+
+    h2d_r_phi_moller[i] = new TH2D("r_phi_moller","r_phi_distribution;phi[rad];Counts",500, 500, 1100, 500, 0, 2*pi); 
+    h2d_rprime_phi_moller[i] = new TH2D("rprime_phi_moller","rprime_phi_distribution;phi[rad];Counts", 500, 0 , 0.1, 200, 0, 2*pi);
+    h2d_phiprime_phi_moller[i] = new TH2D("phiprime_phi_moller","phiprime_phi_distribution;phi[rad];Counts", 500, -0.02 , 0.02, 200, 0, 2*pi);
+
+    Etheta_C12[i] = new TH2D(Form("Etheta_%s_C12",passNames[i]),"E' vs #theta;#theta_{tg}(deg);E'(GeV)",100,0,1.8,100,0,12);
+    Etheta_moller[i] = new TH2D(Form("Etheta_%s_moller",passNames[i]),"E' vs #theta;#theta_{tg}(deg);E'(GeV)",100,0,1.8,100,0,12);
+
     for(int j=0; j<nsectors; j++){
 
-      radial[i][j] = new TH1D(Form("r_%s_%s",passNames[i], sector[j]),"gem_r_distribution;r[mm];Counts",500,500,1300);
-      phi_dist[i][j] = new TH1D(Form("phi_%s_%s",passNames[i], sector[j]),"gem_phi_distribution;r[mm];Counts",500,0,2*pi);
-      rprime[i][j] = new TH1D(Form("gem_rprime_%s_%s",passNames[i], sector[j]),"gem_r'_distribution;r';Counts",1000, 0, 0.1);
+      radial_C12[i][j] = new TH1D(Form("r_C12_%s_%s",passNames[i], sector[j]),"gem_r_distribution;r[mm];Counts",500,500,1300);
+      rprime_C12[i][j] = new TH1D(Form("gem_rprime_C12_%s_%s",passNames[i], sector[j]),"gem_r'_distribution;r';Counts",1000, 0, 0.1);
+
+      radial_moller[i][j] = new TH1D(Form("r_moller_%s_%s",passNames[i], sector[j]),"gem_r_distribution;r[mm];Counts",500,500,1300);
+      rprime_moller[i][j] = new TH1D(Form("gem_rprime_moller_%s_%s",passNames[i], sector[j]),"gem_r'_distribution;r';Counts",1000, 0, 0.1);
 
     }
 
-    double sieve_r, gem_r, gem_ph, rate, tg_th, tg_p, gem_px, gem_py, gem_pz, gem_x, gem_y;
+    double sieve_r_c12, gem_r_c12, gem_ph_c12, rate_c12, tg_th_c12, tg_p_c12, gem_px_c12, gem_py_c12, gem_pz_c12, gem_x_c12, gem_y_c12, main_r_c12;
 
-    T1->SetBranchAddress("sieve_r",&sieve_r);
-    T1->SetBranchAddress("gem1_r",&gem_r);
-    T1->SetBranchAddress("gem1_ph",&gem_ph);
-    T1->SetBranchAddress("gem1_px",&gem_px);
-    T1->SetBranchAddress("gem1_py",&gem_py);
-    T1->SetBranchAddress("gem1_pz",&gem_pz);
-    T1->SetBranchAddress("gem1_x",&gem_x);
-    T1->SetBranchAddress("gem1_y",&gem_y);
-    T1->SetBranchAddress("rate",&rate);
-    T1->SetBranchAddress("tg_th",&tg_th);
-    T1->SetBranchAddress("tg_p",&tg_p);
+    T1->SetBranchAddress("sieve_r",&sieve_r_c12);
+    T1->SetBranchAddress("gem1_r",&gem_r_c12);
+    T1->SetBranchAddress("gem1_ph",&gem_ph_c12);
+    T1->SetBranchAddress("gem1_px",&gem_px_c12);
+    T1->SetBranchAddress("gem1_py",&gem_py_c12);
+    T1->SetBranchAddress("gem1_pz",&gem_pz_c12);
+    T1->SetBranchAddress("gem1_x",&gem_x_c12);
+    T1->SetBranchAddress("gem1_y",&gem_y_c12);
+    T1->SetBranchAddress("rate",&rate_c12);
+    T1->SetBranchAddress("tg_th",&tg_th_c12);
+    T1->SetBranchAddress("tg_p",&tg_p_c12);
+    T1->SetBranchAddress("main_r",&main_r_c12);
 
-    for(int j=0; j<nevents; j++){
+    for(int j=0; j<nevents_c12; j++){
 
       T1->GetEntry(j);
-      int index_sector = -999;
+      int index_sector_c12 = -999;
 
-      rate = rate/400; 
-      if(sieve_r < 26.5) continue;
-      if(gem_ph<0) gem_ph += 2*pi;
+      rate_c12 = rate_c12/(200.); 
+      if(sieve_r_c12 < 26.5) continue;
+      if(gem_ph_c12<0) gem_ph_c12 += 2*pi;
 
-      double r_prime = (gem_x*gem_px + gem_y*gem_py)/(gem_r*gem_pz);
-      double phi_prime = (-gem_y*gem_px+gem_x*gem_py)/(gem_r*gem_pz);
+      double r_prime_c12 = (gem_x_c12*gem_px_c12 + gem_y_c12*gem_py_c12)/(gem_r_c12*gem_pz_c12);
+      double phi_prime_c12 = (-gem_y_c12*gem_px_c12+gem_x_c12*gem_py_c12)/(gem_r_c12*gem_pz_c12);
 
-      hist_r_phi[i]->Fill(gem_r, gem_ph, rate);
-      hist_rprime_phi[i]->Fill(r_prime, gem_ph, rate);
-      hist_phiprime_phi[i]->Fill(phi_prime, gem_ph, rate);
-      Etheta[i]->Fill((tg_th)*(180/pi), tg_p/1000, rate);
+      h2d_r_phi_C12[i]->Fill(gem_r_c12, gem_ph_c12, rate_c12);
+      h2d_rprime_phi_C12[i]->Fill(r_prime_c12, gem_ph_c12, rate_c12);
+      h2d_phiprime_phi_C12[i]->Fill(phi_prime_c12, gem_ph_c12, rate_c12);
+      Etheta_C12[i]->Fill((tg_th_c12)*(180/pi), tg_p_c12/1000, rate_c12);
+
+      main_radial_C12[i]->Fill(main_r_c12, rate_c12);
     
-      for(int l=0; l<nsectors; l++){  if(gem_ph > phi_lo[l] && gem_ph < phi_hi[l]) index_sector = l; }
+      for(int l=0; l<nsectors; l++){  if(gem_ph_c12 > phi_lo[l] && gem_ph_c12 < phi_hi[l]) index_sector_c12 = l; }
 
-      radial[i][index_sector]->Fill(gem_r, rate); 
-      phi_dist[i][index_sector]->Fill(gem_ph, rate); 
-      rprime[i][index_sector]->Fill(r_prime, rate);
+      radial_C12[i][index_sector_c12]->Fill(gem_r_c12, rate_c12); 
+      rprime_C12[i][index_sector_c12]->Fill(r_prime_c12, rate_c12);
 
     }
 
-    c[i]->cd();
-    hist_rprime_phi[i]->SetLineColor(kBlue);
-    hist_rprime_phi[i]->Draw("p");
+    int nevents_moller = T2->GetEntries();
+ 
+    double sieve_r_moller, gem_r_moller, gem_ph_moller, rate_moller, tg_th_moller, tg_p_moller, gem_px_moller, gem_py_moller, gem_pz_moller, gem_x_moller, gem_y_moller, main_r_moller;
+
+    T2->SetBranchAddress("sieve_r",&sieve_r_moller);
+    T2->SetBranchAddress("gem1_r",&gem_r_moller);
+    T2->SetBranchAddress("gem1_ph",&gem_ph_moller);
+    T2->SetBranchAddress("gem1_px",&gem_px_moller);
+    T2->SetBranchAddress("gem1_py",&gem_py_moller);
+    T2->SetBranchAddress("gem1_pz",&gem_pz_moller);
+    T2->SetBranchAddress("gem1_x",&gem_x_moller);
+    T2->SetBranchAddress("gem1_y",&gem_y_moller);
+    T2->SetBranchAddress("rate",&rate_moller);
+    T2->SetBranchAddress("tg_th",&tg_th_moller);
+    T2->SetBranchAddress("tg_p",&tg_p_moller);
+    T2->SetBranchAddress("main_r",&main_r_moller);
+
+    for(int k=0; k<nevents_moller; k++){
+
+      T2->GetEntry(k);
+      int index_sector_moller = -999;
+      rate_moller = rate_moller/(400.);
+      if(sieve_r_moller < 26.5) continue;
+      if(gem_ph_moller<0) gem_ph_moller += 2*pi;
+
+      double r_prime_moller = (gem_x_moller*gem_px_moller + gem_y_moller*gem_py_moller)/(gem_r_moller*gem_pz_moller);
+      double phi_prime_moller = (-gem_y_moller*gem_px_moller+gem_x_moller*gem_py_moller)/(gem_r_moller*gem_pz_moller);
+
+      h2d_r_phi_moller[i]->Fill(gem_r_moller, gem_ph_moller, rate_moller);
+      h2d_rprime_phi_moller[i]->Fill(r_prime_moller, gem_ph_moller, rate_moller);
+      h2d_phiprime_phi_moller[i]->Fill(phi_prime_moller, gem_ph_moller, rate_moller);
+
+      main_radial_moller[i]->Fill(main_r_moller, rate_moller);
+
+      Etheta_moller[i]->Fill((tg_th_moller)*(180/pi), tg_p_moller/1000, rate_moller);
+    
+      for(int l=0; l<nsectors; l++){  if(gem_ph_moller > phi_lo[l] && gem_ph_moller < phi_hi[l]) index_sector_moller = l; }
+
+      radial_moller[i][index_sector_moller]->Fill(gem_r_moller, rate_moller); 
+      rprime_moller[i][index_sector_moller]->Fill(r_prime_moller, rate_moller);
+
+
+    }
+
+    c1[i]->cd();
+    h2d_r_phi_C12[i]->SetLineColor(kBlue);
+    h2d_r_phi_C12[i]->Draw("p");
+    h2d_r_phi_moller[i]->SetLineColor(kGreen);
+    h2d_r_phi_moller[i]->Draw("samep");
 
     for(int ihole = 0; ihole < nholes; ihole++){
 
-      TLine* line1 = new TLine(0, phi_hole_lo[ihole] , 0.1, phi_hole_lo[ihole]);
+      TLine* line1 = new TLine(500 , phi_hole_lo[ihole], 1100, phi_hole_lo[ihole]);
       line1->SetLineColor(kRed);
       line1->SetLineWidth(2);
       line1->Draw("same");
 
     }
 
-    c[i]->Draw();
+    c2[i]->cd();
+    h2d_rprime_phi_C12[i]->SetLineColor(kBlue);
+    h2d_rprime_phi_C12[i]->Draw("p");
+    h2d_rprime_phi_moller[i]->SetLineColor(kGreen);
+    h2d_rprime_phi_moller[i]->Draw("samep");
+
+    c3[i]->cd();
+    h2d_phiprime_phi_C12[i]->SetLineColor(kBlue);
+    h2d_phiprime_phi_C12[i]->Draw("p");
+    h2d_phiprime_phi_moller[i]->SetLineColor(kGreen);
+    h2d_phiprime_phi_moller[i]->Draw("samep");
+
+    for(int ihole = 0; ihole < nholes; ihole++){
+
+      TLine* line1 = new TLine(-0.02, phi_hole_lo[ihole] , 0.02, phi_hole_lo[ihole]);
+      line1->SetLineColor(kRed);
+      line1->SetLineWidth(2);
+      line1->Draw("same");
+
+    }
 
   }
 
-  TFile* fout = new TFile("moller_with_sieve_p5_plots.root", "RECREATE");
+  TFile* fout = new TFile("C12_opticsDS_p2_plots.root", "RECREATE");
   fout->cd();
 
   for(int i=0; i<nFiles; i++){
 
-    hist_r_phi[i]->Write();
-    hist_rprime_phi[i]->Write();
-    hist_phiprime_phi[i]->Write();
-    Etheta[i]->Write();
+    h2d_r_phi_C12[i]->Write();
+    h2d_r_phi_moller[i]->Write();
+
+    main_radial_C12[i]->Write();
+    main_radial_moller[i]->Write();
+
+    h2d_rprime_phi_C12[i]->Write();
+    h2d_rprime_phi_moller[i]->Write();
+
+    h2d_phiprime_phi_C12[i]->Write();
+    h2d_phiprime_phi_moller[i]->Write();
+
+    Etheta_C12[i]->Write();
+    Etheta_moller[i]->Write();
 
     for(int j=0; j<nsectors; j++){
 
-      radial[i][j]->Write();
-      phi_dist[i][j]->Write();
-      rprime[i][j]->Write();
+      radial_C12[i][j]->Write();
+      rprime_C12[i][j]->Write();
+
+      radial_moller[i][j]->Write();
+      rprime_moller[i][j]->Write();
 
     }
 
